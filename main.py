0cb775f2528beacfbef9c56c4bc62f0d7446a721
Madeline <mgorm36@wgu.edu>
2026-01-06 12:19:50 -0500

    Trained random forest in run.py and added train_random_forest step to main.py

--- main.py
+++ main.py
@@ -103,7 +103,20 @@ def go(config: DictConfig):
             # step
 
             ##################
-            # Implement here #
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/src/train_random_forest",
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export"
+                }
+
+            )
             ##################
 
             pass
