223c0b6364065d4f74cfaaf6204de28aacc196a0
Madeline <mgorm36@wgu.edu>
2026-01-06 15:50:13 -0500

    Fixed steps again

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def go(config: DictConfig):
 
         if "basic_cleaning" in active_steps:
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/basic_cleaning",
+                f"{config['main']['components_repository']}/src/basic_cleaning",
                 "main",
                 env_manager="conda",
                 parameters={
@@ -67,7 +67,7 @@ def go(config: DictConfig):
 
         if "data_check" in active_steps:
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/data_check",
+                f"{config['main']['components_repository']}/src/data_check",
                 "main",
                 env_manager="conda",
                 parameters={
@@ -104,7 +104,7 @@ def go(config: DictConfig):
 
             ##################
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/train_random_forest",
+                f"{config['main']['components_repository']}/src/train_random_forest",
                 "main",
                 parameters={
                     "trainval_artifact": "trainval_data.csv:latest",
